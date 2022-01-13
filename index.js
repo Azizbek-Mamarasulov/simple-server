0980a35b9b77025100f435a234a375d597ac7b96
azizbek.mamarasulov <azizbek.mamarasulov@gmail.com>
2022-01-13 18:15:48 +0500

    cors

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const https = require("https");
 
 const express = require("express");
 const { readFileSync } = require("fs");
+const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.json({
@@ -16,8 +19,8 @@ app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 const httpsServer = https.createServer(
   {
-    key: readFileSync("./crt/server-key.pem"),
-    cert: readFileSync("./crt/server-cert.pem"),
+    key: readFileSync("./crt/server-key.pem", "utf-8"),
+    cert: readFileSync("./crt/server-cert.pem", "utf-8"),
   },
   app
 );
